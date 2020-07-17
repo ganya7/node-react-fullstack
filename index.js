2253ad702b8cdabd9d6d2226290790297f3aa00b
arvind <arvillu@gmail.com>
2020-07-16 19:19:48 -0500

    authen, cookie , m4 complete

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 const express = require('express');
+// const passportConfig = require('./services/passport')
+// require('./services/passport');      //the order of requiring User and passport is important as we want
+// to create the model user first which is then used by passport
+require('./models/User');
+require('./services/passport');
+// const authRoutes = require('./routes/authRoutes');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys')
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
-
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-    console.log("at: " + accessToken);
-    console.log("rt: " + refreshToken);
-    console.log("p: " + JSON.stringify(profile));
-    console.log("d: " + done);
-}));
-
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}));
-
-app.get('/auth/google/callback', passport.authenticate('google'));
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,   // 30 days
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+// authRoutes(app);
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
