4e6f566184829b70c2c2d060a7b1deb555549c4c
arvind <arvillu@gmail.com>
2020-07-26 15:03:09 -0500

    before logedin check

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ const mongoose = require('mongoose');
 const keys = require('./config/keys');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+app.use(bodyParser.json());
 app.use(
     cookieSession({
         maxAge: 30 * 24 * 60 * 60 * 1000,   // 30 days
--- models/User.js
+++ models/User.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = Schema({
-    googleID: String
+    googleID: String,
+    credits: {type: Number, default: 0}
 });
 
 mongoose.model('users',userSchema);
