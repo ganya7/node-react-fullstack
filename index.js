ab09033e543b3f8400354c6fbac922c834963598
arvind <arvillu@gmail.com>
2020-07-27 00:15:17 -0500

    fixed v

--- index.js
+++ index.js
@@ -34,9 +34,13 @@ if (process.env.NODE_ENV === 'production') {
 
     // express will serve up the index.html file
     // if it doesnt recognize the route
-    const path = require('path');
+    /* const path = require('path');
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    }); */
+    const root = require('path').join(__dirname, 'client', 'build')
+    app.get("*", (req, res) => {
+        res.sendFile('index.html', { root });
     });
 }
 
