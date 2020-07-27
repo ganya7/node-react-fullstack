064a9ee960fb4feded26331b0570de58e01a59d5
arvind <arvillu@gmail.com>
2020-07-27 14:38:48 -0500

    prod heroku deploy

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -6,7 +6,6 @@ import StripeCheckout from "react-stripe-checkout";
 class Payments extends Component {
     render() {
         // debugger;
-        console.log(("Stripe env: ", process.env.REACT_APP_STRIPE_KEY));
         return (
             <StripeCheckout
                 name="Emaily"
